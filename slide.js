0469de6267b044f9a0334cf06cc1f1a8e6579134
Laiho83 <samir.kabri83@gmail.com>
2014-08-06 10:28:09 +0200

    nova verzija - ob zvoku zascrolla

--- slide.js
+++ slide.js
@@ -1,5 +1,15 @@
-(function(){
+/*Najprej pogledamo, če ima jQuery knjižnico (doda če je potrebno) --> kličemo funkcijo
+getUserMedia() - Prompts the user for permission to use a microphone in v 2 parametru kličemo
+gotStream() z audio stream, This is passed to audioContext.createMediaStreamSource to create
+ a sourceNode that can access your device's audio input.
+ 
+ --- window.AudioContext --> s tem inicializiramo  AudioContext (An audio context controls
+ both the creation of the nodes it contains and the execution of the audio processing, 
+ or decoding. You need to create an AudioContext before you do anything else, as everything 
+ happens inside a context.)*/
 
+(function(){
+	
 	// the minimum version of jQuery we want
 	var v = "1.3.2";
 	var x;
@@ -14,7 +24,6 @@
 			if (!done && (!this.readyState || this.readyState == "loaded" || this.readyState == "complete")) {
 				done = true;
 				getUserMedia({audio:true}, gotStream);
-				//blowScroll();
 			}
 		};
 		document.getElementsByTagName("head")[0].appendChild(script);
@@ -47,21 +56,74 @@
         alert('getUserMedia threw exception :' + e);
 		}
 	}
+	
+	    
+    var analyserNode;
+    var javascriptNode;
+    var sampleSize = 1024;  // number of samples to collect before analyzing
+                            // decreasing this gives a faster sonogram, increasing it slows it down
+    var amplitudeArray;     // array to hold frequency data
+    var audioStream;
+	var column = 0;
+    var canvasWidth  = 800;
+    var canvasHeight = 256;
+    var ctx;
 
 	function gotStream(stream) {
-		// Create an AudioNode from the stream.
-		var mediaStreamSource = audioContext.createMediaStreamSource(stream);
+		 // create the media stream from the audio input source (microphone)
+        sourceNode = audioContext.createMediaStreamSource(stream);
+        audioStream = stream;
 
-		// Connect it to the destination.
+        analyserNode   = audioContext.createAnalyser();
+        javascriptNode = audioContext.createScriptProcessor(sampleSize, 1, 1);
+
+        // Create the array for the data values
+        amplitudeArray = new Uint8Array(analyserNode.frequencyBinCount);
+
+        // setup the event handler that is triggered every time enough samples have been collected
+        // trigger the audio analysis and draw one column in the display based on the results
+        javascriptNode.onaudioprocess = function () {
+
+            amplitudeArray = new Uint8Array(analyserNode.frequencyBinCount);
+            analyserNode.getByteTimeDomainData(amplitudeArray);
+
+			
+			
+            // draw one column of the display
+            drawTimeDomain()
+        }
+
+        // Now connect the nodes together
+        // Do not connect source node to destination - to avoid feedback
+        sourceNode.connect(analyserNode);
+        analyserNode.connect(javascriptNode);
+        javascriptNode.connect(audioContext.destination);
 		
-		blowScroll();
 	}
    	
-	function blowScroll(){
+	 function drawTimeDomain() {
+        var minValue = 9999999;
+        var maxValue = 0;
+
+        for (var i = 0; i < amplitudeArray.length; i++) {
+            var value = amplitudeArray[i] / 256;
 			
+            if(value > maxValue) {
+                maxValue = value;
+            } else if(value < minValue) {
+                minValue = value;
+            }
+        }
+
+        var y_lo = canvasHeight - (canvasHeight * minValue) - 1;
+        var y_hi = canvasHeight - (canvasHeight * maxValue) - 1;
+
+		if(y_hi < 105)
+		{
 			var y = $(window).scrollTop();
-			y = y+1200;
+			y = y+100;
 			jQuery('html,body').animate({scrollTop:y});
+		}
 	}
 	
 
